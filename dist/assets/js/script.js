ee375c03cfa9d9cadf7787d47a00fc15639412a8
knakna <kna@tut.by>
2021-06-29 23:42:43 +0300

    add sections reviews & realize

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,6 +1,19 @@
-function test(a, b) {
-    return a + b
-}
+//  initialize swiper
+
+const swiper = new Swiper('.swiper-container', {
+
+    loop: true,
+
+
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+
+
+  });
 
 console.log(test(5, 2));
 console.log(test(2, 4));
--- src/assets/js/lib.js
+++ src/assets/js/lib.js
@@ -1,3 +1,16 @@
-function test(a, b) {
-    return a + b
-}
+//  initialize swiper
+
+const swiper = new Swiper('.swiper-container', {
+
+    loop: true,
+
+
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+
+
+  });
